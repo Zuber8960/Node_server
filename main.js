a6191496033c8550dc60aa16149077628c0be947
Zuber8960 <zuberahmad8960@gmail.com>
2022-11-27 23:18:15 +0530

    task-8

--- main.js
+++ main.js
@@ -1,45 +1,54 @@
 
 const http = require('http');
+const fs = require('fs');
+const { clear } = require('console');
 const server = http.createServer((req, res) => {
-    // console.log('zuber');
-    // res.write('<H1>Zuber Ahmad</H1>');
-    // res.end();
-    // console.log(req.url,req.headers,req.method);
     const url = req.url;
-   
-    if (url === '/home') {
+    const method = req.method;
+
+    if (url === '/') {
+        fs.readFile("massage.txt", { encoding: "utf-8" }, (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(data);
+            }
+
+            res.write('<html>');
+            res.write('<head><title>Enter Massage</title></head>');
+            res.write(`<body><p>${data}</p><form action="/massage" method="POST"><input type="text" name="massage"><button type="submit">Send</button></form></body>`);
+            res.write('</html>');
+            return res.end();
+        })
+    } else if (url === '/massage' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const massage = parsedBody.split('=')[1];
+            // res.write(massage);
+            fs.writeFile('massage.txt', massage, (err) => {
+                // console.log(err);
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        })
+
+    } else {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
-        res.write('<head><title>Home</title></head>');
-        res.write('<body>Welcome Home</body>');
+        res.write('<head><title>My first page</title></head>');
+        res.write('<body>Welcome to my Node JS projects.</body>');
         res.write('</html>');
-        return res.end()
-    }else if(url==='/about'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>About</title></head>');
-        res.write('<body>About Us</body>');
-        res.write('</html>');
-        return res.end()
-    }else if(url==='/node'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Node</title></head>');
-        res.write('<body>Welcome to my Node JS projects</body>');
-        res.write('</html>');
-        return res.end()
+        res.end();
     }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body>Welcome to my Node JS projects.</body>');
-    res.write('</html>');
-    res.end();
 });
 
-server.listen(4000);
-
-
+server.listen(3000);
 
 
 
